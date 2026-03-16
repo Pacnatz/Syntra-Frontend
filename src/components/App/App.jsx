23c9be9b6f2178dde3e2b272d05068923b3fe038
Pacnatz <nhatchu04@yahoo.com>
2026-03-16 00:16:58 -0500

    create project structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Welcome from "../../pages/Welcome/Welcome";
+
+function App() {
+  return (
+    <div className="app">
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Welcome/Welcome.jsx
@@ -0,0 +1,10 @@
+import "./Welcome.css";
+
+function Welcome() {
+  return (
+    <div className="welcome">
+    </div>
+  );
+}
+
+export default Welcome;
